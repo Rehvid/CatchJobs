6129a452b0be598eaaea40a939e50fab3834f7a7
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-04 21:11:00 +0200

    feat: add to user factory assign roles, and update tests with roles for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\AuthRole;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -21,6 +23,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'phone' => random_int(0, 1) === 0
+                ? null
+                : fake()->unique()->randomNumber(9),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
@@ -34,5 +39,14 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function configure(): UserFactory
+    {
+        return $this->afterCreating(function (User $user) {
+            $user->phone === null
+                ? $user->assignRole(AuthRole::USER->value)
+                : $user->assignRole(AuthRole::EMPLOYER->value);
+        });
     }
 }
--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
+use Database\Seeders\RoleSeeder;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
@@ -23,6 +24,7 @@ class PasswordResetTest extends TestCase
     {
         Notification::fake();
 
+        $this->seed(RoleSeeder::class);
         $user = User::factory()->create();
 
         $this->post('/forgot-password', ['email' => $user->email]);
@@ -34,6 +36,7 @@ class PasswordResetTest extends TestCase
     {
         Notification::fake();
 
+        $this->seed(RoleSeeder::class);
         $user = User::factory()->create();
 
         $this->post('/forgot-password', ['email' => $user->email]);
@@ -51,6 +54,7 @@ class PasswordResetTest extends TestCase
     {
         Notification::fake();
 
+        $this->seed(RoleSeeder::class);
         $user = User::factory()->create();
 
         $this->post('/forgot-password', ['email' => $user->email]);
