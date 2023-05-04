1094671aaced82e0dac53b8c80371ace3a5f9b2e
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-04 19:50:28 +0200

    feat: create company model with migration

---
+++ database/migrations/2023_05_04_172510_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('location_id')->nullable();
+            $table->foreignId('industry_id')->nullable();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->unsignedInteger('vat_number')->unique();
+            $table->unsignedInteger('status')->default(0);
+            $table->text('description')->nullable();
+            $table->string('employees')->nullable();
+            $table->unsignedInteger('foundation_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
