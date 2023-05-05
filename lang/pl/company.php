388d007ed1022cca1545a6b49a4490aa9604daa7
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-05 18:45:26 +0200

    feat: add new translation for company

--- lang/pl/company.php
+++ lang/pl/company.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'list_title' => 'Firmy',
+    'create_title' => 'Podstawowe informacje o twojej firmie',
     'name' => [
         'label' => 'Nazwa firmy',
         'placeholder' => 'XyzCorp48'
@@ -8,7 +9,25 @@ return [
     'industry' => [
         'label' => 'Branża',
         'placeholder' => '',
-        'comment' => 'Kliknij w pole, żeby móc dodać branże.'
+    ],
+    'description' => [
+        'label' => 'Opis firmy',
+    ],
+    'foundation_year' => [
+        'label' => 'Rok założenia',
+        'placeholder' => '1991'
+    ],
+    'employees' => [
+        'label' => 'Liczba Zatrudnionych pracowników',
+        'placeholder' => '25',
+    ],
+    'vat_number' => [
+        'label' => 'NIP',
+        'placeholder' => '123456789'
+    ],
+    'benefits' => [
+        'label' => 'Benefity',
+        'placeholder' => '',
     ],
     'status' => 'Status',
 
