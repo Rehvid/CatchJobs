315ddc0d64513ca2c964a1bd9e22942f919b3e59
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-05 19:58:21 +0200

    feat: create industry migration, model and invokable controller

---
+++ app/Http/Controllers/IndustryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\Industry;
+
+class IndustryController extends Controller
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        return response()->json(Industry::all()->pluck('name'));
+    }
+}
