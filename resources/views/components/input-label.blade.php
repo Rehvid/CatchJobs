8500707d36b9a948340ff5666ace30da08e8908b
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-05 18:44:20 +0200

    refactor: modify anonymous component input

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,15 @@
-@props(['value'])
+@props([
+    'value',
+    'optional' => false
+])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? $slot }}
-</label>
+@if ($optional)
+    <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+        <span class="pr-2">{{ $value }}</span>
+        <span>{{ __('(optional)') }}</span>
+    </label>
+@else
+    <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+        {{ $value ?? $slot }}
+    </label>
+@endif
