f2150011b95981a9c2f5fe5ad6a15d0e197e8427
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-05 20:00:06 +0200

    feat: create export function for getting data from database

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,27 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+export function getDataFromServer(url) {
+    if (!url) {
+        return console.error('Url is not provided');
+    }
+
+    const csrfToken = document.querySelector('meta[name="csrf-token"]') || false;
+
+    if (!csrfToken) {
+        return  console.error('Csrf token is not found')
+    }
+
+    return fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*",
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRF-TOKEN": csrfToken.getAttribute('content'),
+        },
+        method: 'get',
+        credentials: "same-origin",
+    })
+}
