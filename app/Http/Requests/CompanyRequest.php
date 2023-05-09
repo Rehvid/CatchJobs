57063d8cb13fcfd642944a88262ac4015e7cbd73
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-09 04:49:59 +0200

    feat: create companyRequest for company form

---
+++ app/Http/Requests/CompanyRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Company;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use JsonException;
+
+class CompanyRequest extends FormRequest
+{
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:2', 'max:255', 'unique:'.Company::class],
+            'vat_number' => ['required', 'string', 'size:10', 'unique:'.Company::class],
+            'description' => ['nullable', 'string', 'max:1000'],
+            'employees' => ['nullable', 'string'],
+            'foundation_year' => ['nullable', 'integer', 'digits:4', 'gt:0'],
+            'benefits' => ['nullable', 'array'],
+            'benefits.*.value' => ['required','string', 'min:2','max:255'],
+            'industry' => ['nullable', 'string', 'min:2', 'max:255']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'benefits.*.value' => 'The benefit must be string'
+        ];
+    }
+
+
+    protected function prepareForValidation(): void
+    {
+        if ($this->benefits && Str::isJson($this->benefits)) {
+            try {
+               $this->merge([
+                   'benefits' => json_decode($this->benefits, true, 512, JSON_THROW_ON_ERROR),
+               ]);
+            } catch (JsonException) {
+                throw ValidationException::withMessages(['benefits' => __('validation.wrong_syntax')]);
+            }
+         }
+    }
+}
