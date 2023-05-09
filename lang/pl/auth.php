a13420d4f5b7282a124fbcebd37ae5fd13369ba7
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-09 04:55:26 +0200

    feat: add language files

---
+++ lang/pl/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Sprawdź, czy adres e-mail i hasło są poprawne',
+    'password' => 'Hasło jest nieprawidłowe.',
+    'throttle' => 'Za dużo nieudanych prób logowania. Proszę spróbować za :seconds sekund.',
+
+    'already_register' => 'Posiadasz już konto?',
+    'forget_password' => 'Zapomniałeś hasła?',
+    'remember_me' => 'Zapamiętaj mnie',
+];
