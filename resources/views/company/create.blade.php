869e845cf86609ae2045dd2468faa6a69436885a
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-12 05:18:43 +0200

    feat: locations variable passed to location inputs view

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -4,13 +4,21 @@
             <form class="flex flex-wrap mb-6 p-4 sm:p-8 bg-white shadow sm:rounded-lg" method="post" action="{{ route('companies.store') }}">
                 @csrf
 
+
                 <div class="w-full">
                     @include('company.partials.company-inputs')
+                </div>
+
+                <div class="w-full lg:mt-10">
+                    @include('company.partials.location-inputs', ['locations' => $locations])
                 </div>
 
                 <div class="flex items-center gap-4 px-4">
                     <x-primary-button class="w-full bordered-full">{{ __('company.create') }}</x-primary-button>
                 </div>
+
+
+
             </form>
 
 
