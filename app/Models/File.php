924c54389e6b6ab97d1181f5c5394719542fd0e7
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-13 16:54:27 +0200

    feat: create migration for files and pivot table for company also create model for file

---
+++ app/Models/File.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'disk',
+        'path',
+        'mime_type',
+        'collection'
+    ];
+}
