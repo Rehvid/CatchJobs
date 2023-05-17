c44766c00e0ea2f1003345a4157dee9ce5799565
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-17 12:57:07 +0200

    feat: add new methods for relation to company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Mews\Purifier\Casts\CleanHtmlInput;
 
@@ -35,7 +36,7 @@ class Company extends Model
         'description' =>  CleanHtmlInput::class,
     ];
 
-    protected $with = ['industry'];
+    protected $with = ['industry', 'location'];
 
     public function getRouteKeyName(): string
     {
@@ -57,6 +58,11 @@ class Company extends Model
         return $this->belongsTo(Industry::class);
     }
 
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
     public function socials(): HasMany
     {
         return $this->hasMany(Social::class);
@@ -65,5 +71,36 @@ class Company extends Model
     public function files(): BelongsToMany
     {
         return $this->belongsToMany(File::class);
+    }
+
+    public function socialByNetworkId(int $socialNetworkId): ?string
+    {
+        $socials = $this->socials()->get();
+
+        return $socials
+            ->filter(fn (?Social $social) => $social->social_network_id === $socialNetworkId)
+            ?->value('url');
+    }
+
+    public function getImageByCollection(string $collection): ?string
+    {
+        $files = $this->files()->get();
+
+        $pathToImage =  $files->filter( fn(?File $file) => $file->collection === $collection)?->value('path');
+
+        if ($pathToImage) {
+            return url('/' . 'storage/' . $pathToImage);
+        }
+
+        return null;
+    }
+
+    public function getGalleryImages(): Collection
+    {
+        $files = $this->files()->get();
+
+        $galleries = $files->filter(fn(?File $file) => $file->collection === 'gallery');
+
+        return $galleries->map(fn (File $file) => url('/' . 'storage/' . $file->path));
     }
 }
