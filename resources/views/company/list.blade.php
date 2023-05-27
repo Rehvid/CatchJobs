c873b4d70aebf36a057e325639b220f37cdce668
Dawid Harendarz <dharendarz2@gmail.com>
2023-05-27 07:38:54 +0200

    feat: add displaying status message for employer if company is rejected to show

--- resources/views/company/list.blade.php
+++ resources/views/company/list.blade.php
@@ -63,7 +63,27 @@
 
                                     </th>
                                     <td class="px-6 py-4">
-                                        <x-status-badge status="{{ $company->status }}"/>
+                                        @if ($company->status === \App\Enums\Status::REJECT)
+                                            <div class="flex">
+                                                <x-status-badge status="{{ $company->status }}" />
+                                                <span class="w-5 block " data-tooltip-target="tooltip-status{{ $company->id }}">
+                                                        <svg fill="none" stroke="currentColor" stroke-width="1.5"
+                                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                                             aria-hidden="true">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z">
+                                                            </path>
+                                                        </svg>
+                                                    </span>
+                                                <div id="tooltip-status{{ $company->id }}" role="tooltip"
+                                                     class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                                    {!! clean($company->status_message) !!}
+                                                    <div class="tooltip-arrow" data-popper-arrow></div>
+                                                </div>
+                                            </div>
+                                        @else
+                                            <x-status-badge status="{{ $company->status }}" />
+                                        @endif
                                     </td>
                                     <td class="px-6 py-4">
                                         {{ $company->industry?->name }}
